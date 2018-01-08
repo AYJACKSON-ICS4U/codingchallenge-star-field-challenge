7c04d34bb2b87793a4fa68be26375bae993d1372
Jmichelle1 <31673374+Jmichelle1@users.noreply.github.com>
2018-01-08 10:27:30 -0500

    Created file

---
+++ Star.js
@@ -0,0 +1,19 @@
+Star[] stars = new Star [800];
+
+
+float speed;
+void setup() {
+size(800, 800);
+for (int i = 0; i < stars.length; i++){
+  stars[i] = new Star();
+}
+}
+void draw() {
+speed = map(mouseX, 0, width, 0, 20);
+background(0, 0, 0);
+translate(width/2, height/2);
+for (int i = 0; i < stars.length; i++){
+  stars[i] = update();
+  stars[i].show();
+}
+}
