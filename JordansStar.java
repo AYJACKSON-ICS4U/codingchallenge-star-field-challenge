3ff7fc8dbded9009b524d0e0a96ac0e86d35d01f
Jmichelle1 <31673374+Jmichelle1@users.noreply.github.com>
2018-01-10 10:28:51 -0500

    Created file

---
+++ JordansStar.java
@@ -0,0 +1,19 @@
+Star[] stars = new Star [400];
+
+float speed;
+void setup() {
+  //fullScreen();
+  //size(400, 400);
+  for (int i = 0; i < stars.length; i++){
+    stars[i] = new Star();
+  }
+}
+void draw() {
+  speed = map(mouseX, 0, width, 0, 20);
+  background(0, 0, 0);
+  translate(width/2, height/2);
+  for (int i = 0; i < stars.length; i++){
+    stars[i].update();
+    stars[i].show();
+  }
+}
